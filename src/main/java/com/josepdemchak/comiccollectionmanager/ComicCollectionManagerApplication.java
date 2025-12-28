4ab0bcafd628604e2d79aa3f898c916b8c95a148
jdemchak <jcdemchak@gmail.com>
2025-12-27 19:40:01 -0500

    Initial Spring Boot project

---
+++ src/main/java/com/josepdemchak/comiccollectionmanager/ComicCollectionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.josepdemchak.comiccollectionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicCollectionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicCollectionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josepdemchak/comiccollectionmanager/ComicCollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josepdemchak.comiccollectionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicCollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
